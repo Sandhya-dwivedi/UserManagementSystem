68186ed99a725a7b7c74c06c00ad5cea5d374d6e
Sandhya-dwivedi <dwivedisandhya389@.com>
2025-01-15 11:39:55 +0530

    First commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Index from './components/Index';
+import Routing from './components/Routing';
+import About from './components/About';
+function App() {
+  return (
+    <div className="App">
+     <Routing />
+     {/* <About/> */}
+    </div>
+  );
+}
+
+export default App;
